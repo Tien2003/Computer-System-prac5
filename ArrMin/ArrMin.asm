a459f35e9622b2aa009b1a723582641b66042438
Tien <autien0809@gmail.com>
2024-09-19 20:24:32 +0930

    rw

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -2,3 +2,56 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//smallest <- arr[0] 
+@R1
+A=M
+D=M
+@smallest
+M=D
+
+@R2
+M=M-1
+
+(LOOP)
+    @R2
+    D=M
+
+    @END
+    D;JLE
+
+    @R2
+    D=M
+    @R1
+    A=M+D
+    D=M
+
+    @smallest
+    D=D-M //if current<smallest
+    @UPDATE
+    D;JLT
+
+    @R2
+    M=M-1
+    @LOOP
+    0;JMP
+
+
+(UPDATE)
+    @R1
+    D=M
+    @R2
+    A=D+M
+    D=M
+    @smallest
+    M=D
+
+(END)
+    @smallest
+    D=M
+    @R0
+    M=D
+
+(INFINITE)
+    @INFINITE
+    0;JMP
