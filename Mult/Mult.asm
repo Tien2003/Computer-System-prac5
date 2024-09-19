c5dea5beaab8948dd159571c2f5bc74c0f569c6f
Tien <autien0809@gmail.com>
2024-09-19 15:16:54 +0930

    sdf

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,17 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R1
+D=M
+@NEG
+D;JLT
+
+@LOOP
+0;JMP
+
 (LOOP)
     @R1
     MD=M-1
+
     @END
     D;JLT
+
     @R2
     D=M
     @R0
     M=M+D
     @LOOP
-    M;JMP
+    0;JMP  
+
+(NEG)
+    @R1
+    M=-M
+
+    @8
+    M=1
+
+    @LOOP
+    0;JMP  
 (END)
+    @8
+    D=M
+    @FINAL
+    D;JEQ
+
+    @R0
+    M=-M
+
+(FINAL)
     @END
     0;JMP
+
