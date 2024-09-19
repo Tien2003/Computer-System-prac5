010a913236ceb9ae55232c92d2fc61bbce2b2bdd
Tien <autien0809@gmail.com>
2024-09-19 19:16:20 +0930

    w

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -7,47 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
 @R0
 M=0
-
-// Initialize NEG_FLAG to 0
-@NEG_FLAG
+@8
 M=0
 
-// Check if R1 is negative
 @R1
 D=M
-@PROCESS_R1
-D;JGE
+@NEG
+D;JLT
 
-@R1
-M=-M
-@NEG_FLAG
-M=1
-
-(PROCESS_R1)
 (LOOP)
     @R1
-    D=M
+    MD=M-1
     @END
-    D;JEQ  // Jump to END if R1 == 0
+    D;JEQ 
 
     @R2
     D=M
     @R0
-    M=M+D 
+    M=M+D   
 
+    @LOOP
+    0;JMP
+
+(NEG)
     @R1
-    M=M-1  
+    M=-M
+
+    @8
+    M=1
 
     @LOOP
     0;JMP
 
 (END)
-    @NEG_FLAG
+    @8
     D=M
     @FINAL
     D;JEQ  
+
     @R0
     M=-M
 
